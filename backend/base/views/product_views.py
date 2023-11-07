2824e057372c24a84213bdf0c890d199ea193961
DuyHuy <ninhduyhuy2001@gmail.com>
2023-11-07 13:05:26 +0700

    product_list_admin_screen

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from base.models import Product
@@ -20,3 +21,53 @@ def getProduct(request, pk):
     serializer = ProductSerializer(product, many=False)
 
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    user = request.user
+
+    product = Product.objects.create(
+        user=user,
+        name='Sample Name',
+        price=100,
+        brand='Test brand',
+        countInStock=0,
+        category='Sample category',
+        description='',
+    )
+
+    serializer = ProductSerializer(product, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def updateProduct(request, pk):
+    data = request.data
+    product = Product.objects.get(_id=pk)
+
+    product.name = data['name']
+    product.price = data['price']
+    product.brand = data['brand']
+    product.countInStock = data['countInStock']
+    product.category = data['category']
+    product.description = data['description']
+
+    product.save()
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    try:
+        product = Product.objects.get(_id=pk)
+        product.delete()
+        return Response({"message": f"Delete {product.name}"})
+
+    except:
+        return Response({"error": "Product not Found"}, status=404)
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReducer, productDetailsReducer } from "./reducers/productReducers";
+import {
+	productListReducer,
+	productDetailsReducer,
+	productDeleteReducer,
+	productCreateReducer,
+} from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import {
 	userLoginReducer,
@@ -23,6 +28,9 @@ import {
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
+	productDelete: productDeleteReducer,
+	productCreate: productCreateReducer,
+
 	cart: cartReducer,
 	userLogin: userLoginReducer,
 	userRegister: userRegisterReducer,
